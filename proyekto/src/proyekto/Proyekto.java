eacf3e271d9e1acd6fa310f2fa9fcdafeb8ed096
snaf0o <eksborlongan@gmail.com>
2023-03-04 00:10:32 +0800

    basic lang pala to kala ko naman

--- proyekto/src/proyekto/Proyekto.java
+++ proyekto/src/proyekto/Proyekto.java
@@ -14,7 +14,8 @@ public class Proyekto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        login A = new login(); 
+        A.setVisible(true);
     }
     
 }
